b33842112bece50c1f1ebf0e78d67fa986a986e8
KlicekNina <33865914+KlicekNina@users.noreply.github.com>
2018-05-22 09:33:49 +0200

    Add files via upload

---
+++ Zagonski.java
@@ -0,0 +1,12 @@
+package si.feri.ost.ost.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zagonski {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Zagonski.class, args);
+	}
+}
