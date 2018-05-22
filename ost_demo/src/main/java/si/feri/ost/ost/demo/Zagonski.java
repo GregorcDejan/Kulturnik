f9bc800fdd04f0cbb90f924cd4f20435ce30352f
KlicekNina <33865914+KlicekNina@users.noreply.github.com>
2018-05-22 11:25:09 +0200

    Test2

--- ost_demo/src/main/java/si/feri/ost/ost/demo/Zagonski.java
+++ ost_demo/src/main/java/si/feri/ost/ost/demo/Zagonski.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Zagonski {
 
+	//testSprememb
+	//test2
 	public static void main(String[] args) {
 		SpringApplication.run(Zagonski.class, args);
 	}
