eac2ac94f4e2c10424cf643931a14cdc49b9af16
GregorcDejan <gregorc.dejan@gmail.com>
2018-05-22 11:39:22 +0200

    ParsingRazredi

---
+++ ost_demo/src/main/java/si/feri/ost/ost/demo/Razredi/Oseba.java
@@ -0,0 +1,4 @@
+package si.feri.ost.ost.demo.Razredi;
+
+public class Oseba {
+}
--- ost_demo/src/main/java/si/feri/ost/ost/demo/Zagonski.java
+++ ost_demo/src/main/java/si/feri/ost/ost/demo/Zagonski.java
@@ -2,11 +2,13 @@ package si.feri.ost.ost.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import si.feri.ost.ost.demo.XMLParsing.XMLParser;
 
 @SpringBootApplication
 public class Zagonski {
 
 	public static void main(String[] args) {
+		new XMLParser();
 		SpringApplication.run(Zagonski.class, args);
 	}
 }
