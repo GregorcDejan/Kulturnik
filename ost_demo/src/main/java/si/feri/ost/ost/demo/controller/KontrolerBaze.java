a0b35fe4e8db84e3a76d1406ec30564726c3443c
GregorcDejan <gregorc.dejan@gmail.com>
2018-06-13 09:10:17 +0200

    parsing++

--- ost_demo/src/main/java/si/feri/ost/ost/demo/controller/KontrolerBaze.java
+++ ost_demo/src/main/java/si/feri/ost/ost/demo/controller/KontrolerBaze.java
@@ -732,7 +732,7 @@ public class KontrolerBaze {
     }
 
 
-    @RequestMapping(value = {"/downXML"}, method = RequestMethod.GET)
+    @RequestMapping(value = {"/downXML"}, method = RequestMethod.POST)
     @ResponseBody
     public String testing123(Model model) {
         List<Dogodek> dogodk=dogodki.getAllDogodki();
@@ -742,52 +742,7 @@ public class KontrolerBaze {
 
 
 
-        /*DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-        DocumentBuilder db = null;
 
-        try {
-            URL url=new URL("https://www.kolosej.si/spored/xml/2.0/");
-            db = dbf.newDocumentBuilder();
-            Document doc = db.parse(url.openStream());
-            doc.getDocumentElement().normalize();
-            System.out.println("Root element: " + doc.getDocumentElement().getNodeName());
-            NodeList nList = doc.getElementsByTagName("movie");
-            System.out.println("--------------------------------------------------------------------------------------------------------------------------");
-            for (int temp = 0; temp < nList.getLength(); temp++) {
-
-                Node nNode = nList.item(temp);
-
-                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-
-                    Element eElement = (Element) nNode;
-                    String naziv=eElement.getElementsByTagName("title").item(0).getTextContent();
-
-                    String vir=eElement.getElementsByTagName("url").item(0).getTextContent();
-
-                    String virURL=eElement.getElementsByTagName("poster").item(0).getTextContent().trim();
-
-                    String opis=eElement.getElementsByTagName("plot_outline").item(0).getTextContent();
-                    String datum=eElement.getElementsByTagName("date").item(0).getTextContent();
-                    System.out.println(datum);
-
-                    String ura =eElement.getElementsByTagName("time").item(0).getTextContent();
-                    String lokacija = eElement.getElementsByTagName("center").item(0).getTextContent();
-                    String kraj = eElement.getElementsByTagName("city").item(0).getTextContent();
-                    String izvajalec=eElement.getElementsByTagName("distributor").item(0).getTextContent();
-                    //dogod.add(new Dogodek(naziv,kraj,ura,"Kolosej",lokacija,6,opis,virURL,"Kino",datum,vir));
-                    //dogodki.add(new Dogodek(naziv, kraj, ura, izvajalec, lokacija, "6", opis, virURL,1, "Film", datum, vir));
-
-                }
-
-            }
-        } catch (ParserConfigurationException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (SAXException e) {
-            e.printStackTrace();
-        }
-        */
         for (Dogodek dogodek : dogodk) {
             System.out.println(dogodek);
 
@@ -820,17 +775,18 @@ public class KontrolerBaze {
         }
         finally {}
 
-
+        zaZapisat+="  ";
         System.out.println("_____________________________________________________________________________");
         System.out.println(zaZapisat);
-        /*try {
-            //FileWriter fw = new FileWriter(file);
-            //fw.write(zaZapisat);
+        try {
+            FileWriter fw = new FileWriter(file);
+            fw.write(zaZapisat);
         } catch (IOException e) {
-            // TODO Auto-generated catch block
+
             e.printStackTrace();
-        }*/
+        }
         return zaZapisat;
+
     }
 
 
